92ef1330fe5f7ed551d1fe218b732506cc282a02
anpdko <mrstr91@gmail.com>
2023-04-06 00:19:41 +0300

    about

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -4,15 +4,13 @@ import { useTranslation } from 'react-i18next';
 import { useGsapFrom } from '../../hooks/useGsap'
 import { useSelector } from 'react-redux'
 import { RootState } from '../../store/store';
+import {list, filter} from './data';
 
 
 
 const About = () => {
    const { t } = useTranslation();
    const { directionScroll } = useSelector((state: RootState) => state.gsap)
-
-   const filter = [t("all"), t("web"), 'react', t("social"), 
-   t("animation"), 'backend', t("design"), t("database"), t("more")];
 
    const refAbout = useGsapFrom({
       opacity: 0, y: 150 * directionScroll
@@ -25,67 +23,17 @@ const About = () => {
          <p className='gray'>{t("Welcome to my portfolio website! I am a Front-End developer specializing in creating functional web applications using React.")}</p>
          <p className='gray'>{t("Currently, I am looking for an opportunity to join a professional and friendly team. I am ready to work hard towards becoming a professional and achieving my goals. I hope you enjoy my work and see my experience and skills.")}</p>
 
-         <h4 className="blue">{t("FILTER SKILLS")}</h4>
+         <h4 className="blue">{t("SKILLS")}</h4>
          <div className={s.filter_skills}>
+            <span>#{t('all')}</span>
             {filter.map(item => 
-               <span>#{item}</span>
+               <span key={item.id}>#{t(item.title)}</span>
             )}
          </div>
-
-         <h4 className="blue">{t("LIST SKILLS")}</h4>
          <div className={s.row_skils}>
-            <span>HTML</span>
-            <span>CSS</span>
-            <span>SCSS</span>
-            <span>{t("Responsive")}</span>
-            <span>Grid</span>
-            <span>HTML</span>
-            <span>JavaScript</span>
-            <span>TypeScript</span>
-            <span>ES6</span>
-            <span>React</span>
-            <span>Redux-toolkit</span>
-            <span>RestApi</span>
-            <span>Framer Motion</span>
-            <span>Gsap</span>
-            <span>{t("Communicative")}</span>
-            <span>{t("Team-oriented")}</span>
-            <span>Git</span>
-            <span>Jira</span>
-            <span>Express.js</span>
-            <span>Postman</span>
-            <span>MongoDB</span>
-            <span>Firebase</span>
-            <span>{t("Mentoring")}</span>
-            <span>{t("Public speaking")}</span>
-            <span>React Native</span>
-            <span>UI/UX design</span>
-            <span>Figma</span>
-            <span>Python</span>
-
-            {/* <div className={s.col_skils}>
-               <h4 className="blue">SKILLS</h4>
-               <ul>
-                  <li>HTML, CSS, SCSS</li>
-                  <li>Responsive, Flex, Grid</li>
-                  <li>JavaScript, TypeScript, ES6</li>
-                  <li>React, Redux-toolkit, RestApi</li>
-                  <li>Framer Motion, Gsap</li>
-                  <li>Communicative, Team-oriented</li>
-                  <li>Git, Jira</li>
-               </ul>
-            </div>
-            <div className={s.col_skils}>
-               <h4 className="blue">EXTRA SKILLS</h4>
-               <ul>
-                  <li>Express.js, Postman</li>
-                  <li>MongoDB, Firebase</li>
-                  <li>Mentoring, Public speaking</li>
-                  <li>React Native</li>
-                  <li>UI/UX design, Figma</li>
-                  <li>Python</li>
-               </ul>
-            </div> */}
+            {list.map((item) => 
+               <span key={item.id}>{item.name} /</span>
+            )}
          </div>
       </div>
    );
---
+++ src/pages/About/data.ts
@@ -0,0 +1,43 @@
+
+const filter = [
+   { id: 0, title: 'web', list: [] },
+   { id: 1, title: 'react', list: [] },
+   { id: 3, title: 'back-end', list: [] },
+   { id: 2, title: 'social', list: [] },
+   { id: 4, title: 'design', list: [] },
+   { id: 5, title: 'more', list: [] },
+]
+
+
+
+const list = [
+   { id: 0, name: 'HTML' },
+   { id: 1, name: 'CSS' },
+   { id: 2, name: 'SCSS' },
+   { id: 3, name: 'Responsive' },
+   { id: 4, name: 'JavaScript' },
+   { id: 5, name: 'TypeScript' },
+   { id: 6, name: 'React' },
+   { id: 7, name: 'Redux-toolkit' },
+   { id: 8, name: 'RestApi' },
+   { id: 9, name: 'Framer Motion' },
+   { id: 10, name: 'Gsap' },
+   { id: 11, name: 'Communicative' },
+   { id: 12, name: 'Team-oriented' },
+   { id: 13, name: 'Git' },
+   { id: 14, name: 'Jira' },
+   { id: 15, name: 'Express.js' },
+   { id: 16, name: 'Postman' },
+   { id: 17, name: 'MongoDB' },
+   { id: 18, name: 'Firebase' },
+   { id: 19, name: 'Mentoring' },
+   { id: 20, name: 'Public speaking' },
+   { id: 21, name: 'React Native' },
+   { id: 22, name: 'UI/UX design' },
+   { id: 23, name: 'Figma' },
+   { id: 24, name: 'Python' },
+   { id: 25, name: 'Django' },
+]
+
+
+export { list, filter }
