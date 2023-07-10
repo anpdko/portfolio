5915259d52428a97f2aa558b5ef72f0ad04b970f
Andrii <anpdko@gmail.com>
2023-07-11 01:01:21 +0300

    isOnTopScroll

--- src/components/ScrollNextPage.tsx
+++ src/components/ScrollNextPage.tsx
@@ -8,20 +8,27 @@ interface INavigateScroll {
 
 
 const ScrollNextPage = ({ children, id }: INavigateScroll) => {
-   const { ref, inView } = useInView({ threshold: 0.1 });
+   const { ref, inView, entry } = useInView({ threshold: 0.1 });
 
     useEffect(() => {
       if(inView){
          const element = document.getElementById(id);
          if (element) {
-            const scrollTop = element.offsetTop
+            const isOnTopScroll = entry?.boundingClientRect.y
+            let scrollTop = 0;
+            if(Number(isOnTopScroll) < 0){
+               scrollTop = element.offsetTop + element.offsetHeight - window.innerHeight;
+            }
+            else {
+               scrollTop = element.offsetTop
+            }
             window.scrollTo({
-              top: scrollTop,
-              behavior: 'smooth'
-            });
+               top: scrollTop,
+               behavior: 'smooth'
+             });
           }
       }
-    }, [inView, id])
+    }, [inView, id, entry?.boundingClientRect.y])
 
    return (
       <div ref = {ref} style={{width: "100%"}}>
