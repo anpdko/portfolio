286ce1b0eb73956bf4723ecfefbeabce504eb2e0
Andreii Pryadko <50361705+anpdko@users.noreply.github.com>
2023-11-02 17:28:12 +0200

    Update data.ts

--- src/pages/About/data.ts
+++ src/pages/About/data.ts
@@ -14,6 +14,7 @@ const listData:IListData[] = [
    { id: 5, name: 'TypeScript', rating: 80},
    { id: 6, name: 'React', rating: 95},
    { id: 7, name: 'Redux-toolkit', rating: 90},
+   { id: 26, name: 'Three JS', rating: 90},
    { id: 8, name: 'RestApi', rating: 90},
    { id: 9, name: 'Framer Motion', rating: 75},
    { id: 10, name: 'Gsap', rating: 75},
@@ -32,6 +33,12 @@ const listData:IListData[] = [
    { id: 23, name: 'Figma', rating: 80},
    { id: 24, name: 'Python', rating: 70},
    { id: 25, name: 'Django', rating: 60},
+   { id: 27, name: 'OOP', rating: 60},
+   { id: 28, name: 'Expo', rating: 60},
+   { id: 29, name: 'Vue.js', rating: 50},
+   { id: 30, name: 'Stripe Pay', rating: 50},
+   { id: 31, name: 'Bitbucket', rating: 50},
+   { id: 32, name: 'Scrum', rating: 50},
 ]
 
 
@@ -49,12 +56,12 @@ for (let i = 0; i < listData.length; i++){
 
 const filterData:IFilterData[] = [
    { id: 0, title: 'all', list: allListData },
-   { id: 1, title: 'web', list: [0, 1, 2, 3, 4, 5, 10] },
-   { id: 2, title: 'react', list: [4, 5, 6, 7, 8, 9, 10] },
-   { id: 3, title: 'back-end', list: [15, 16, 17, 18] },
-   { id: 4, title: 'social', list: [11, 12, 13, 14, 19, 20] },
+   { id: 1, title: 'web', list: [0, 1, 2, 3, 4, 5, 10, 26, 29, 30] },
+   { id: 2, title: 'react', list: [4, 5, 6, 7, 8, 9, 10, 26, 30] },
+   { id: 3, title: 'back-end', list: [15, 16, 17, 18, 30] },
+   { id: 4, title: 'social', list: [11, 12, 13, 14, 19, 20, 31, 32] },
    { id: 5, title: 'design', list: [22, 23] },
-   { id: 6, title: 'more', list: [21, 24, 25] },
+   { id: 6, title: 'more', list: [21, 24, 25, 27, 28, 30] },
 ]
 
 
